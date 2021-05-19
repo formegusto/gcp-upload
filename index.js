3fc7403b50e01df3048ba25cd3ed1db368f9da9e
formegusto <formegusto@gmail.com>
2021-05-19 22:04:38 +0900

    ()

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const storage = require("@google-cloud/storage");
+const storage = require("@google-cloud/storage")();
 const sharp = require("sharp");
 
 exports.resizeAndUpload = (data, context) => {
   const { bucket, name } = data;
+  console.log(name, name);
   const ext = name.split(".")[name.split(".").length - 1];
   const requiredFormat = ext === "jpg" ? "jpeg" : ext;
   // sharp 에서는 jpg 대신 jpeg를 사용합니다.
   console.log("name", name, "ext", ext);
+  console.log("Key", Key);
 
-  const file = new storage.Bucket(bucket).file(name);
+  const file = storage.bucket(bucket).file(name);
   const readStream = file.createReadStream();
 
-  const newFile = new storage.Bucket(bucket).file(`thumb/${name}`);
+  const newFile = storage.bucket(bucket).file(`thumb/${name}`);
   const writeStream = newFile.createWriteStream();
 
   sharp(readStream)
